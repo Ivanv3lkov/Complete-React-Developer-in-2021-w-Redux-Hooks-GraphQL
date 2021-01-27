8572162c5abf435e8826e7c1f2e70618546cce38
Ivan Velkov <ivanv3lkov@gmail.com>
2021-01-27 20:28:16 +0200

    lesson 8

--- lesson-8/src/components/form-input/form-input.component.jsx
+++ lesson-8/src/components/form-input/form-input.component.jsx
@@ -6,13 +6,9 @@ const FormInput = ({ handleChange, label, ...otherProps }) => (
   <div className='group'>
     <input className='form-input' onChange={handleChange} {...otherProps} />
     {label ? (
-      <label
-        className={`${
-          otherProps.value.length ? 'shrink' : ''
-        } form-input-label`}
-      >
-        {label}
-      </label>
+    <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+      {label}
+    </label>
     ) : null}
   </div>
 );
