71b3b5abe515c0ea09891aae8a00b10c22994381
Ivan Velkov <ivanv3lkov@gmail.com>
2021-01-27 09:42:10 +0200

    lesson 2

---
+++ lesson-2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import HomePage from './homepage.component';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
