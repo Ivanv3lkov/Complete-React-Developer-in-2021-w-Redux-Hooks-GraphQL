0256ed2754d9180939bfdd8b1bf230225b85a239
Ivan Velkov <ivanv3lkov@gmail.com>
2021-01-28 11:29:59 +0200

    lesson 8

--- lesson-8/src/components/menu-item/menu-item.component.jsx
+++ lesson-8/src/components/menu-item/menu-item.component.jsx
@@ -11,7 +11,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div
       className='background-image'
       style={{
-        backgroundImage: `url(${imageUrl})`
+      backgroundImage: `url(${imageUrl})`
       }}
     />
     <div className='content'>
