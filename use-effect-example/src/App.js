bc17e1b116fe87f378ec046bed6e6669d2fb0da3
Ivan Velkov <ivanv3lkov@gmail.com>
2021-02-03 20:50:39 +0200

    useEffect example

---
+++ use-effect-example/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import UseEffectExample from './components/use-effect-example/use-effect-example.component';
+
+import './App.css';
+
+const App = props => {
+  return <UseEffectExample />;
+};
+
+export default App;
